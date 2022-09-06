8db57f5c178d4016a091b1fa90d1455514b99913
subas <subas@LAPTOP-C1FQQ50G>
2022-09-06 15:08:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // importing namespace using using keyword it contains more than one class inside it
+
+
+namespace Namespace
+{
+    class Program  // here program is the class name  a class can contain more than one method
+    {
+        static void Main(string[] args) 
+            // here Main it is the method how we are considering it is method because after this we have ()(parenthesis)
+            // and its return type is void it means it wont return anything 
+        {  // it is called curely braces
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
