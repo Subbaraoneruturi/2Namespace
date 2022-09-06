608b2ac924fcf56a0cd996d1f43ff6373922ae33
subas <subash.neruturi@gmail.com>
2022-09-06 15:14:05 +0530

    added a line

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Namespace
 {
     class Program  // here program is the class name  a class can contain more than one method
     {
-        static void Main(string[] args) 
+static void Main(string[] args) 
             // here Main it is the method how we are considering it is method because after this we have ()(parenthesis)
             // and its return type is void it means it wont return anything 
         {  // it is called curely braces
